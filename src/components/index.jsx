eafae1bf3873a929e3ec7706ae64c232d6f19c98
Jigar Dafda <jigar.dafda@gmail.com>
2024-08-06 17:50:12 +0000

    [Auto Generated]

---
+++ src/components/index.jsx
@@ -0,0 +1,16 @@
+import PriceBreakup from "./price-breakup/price-breakup";
+import Comment from "../page-layouts/cart/Components/comment/comment";
+import GstCard from "../page-layouts/cart/Components/gst-card/gst-card";
+import ShareCart from "../page-layouts/cart/Components/share-cart/share-cart";
+import DeliveryLocation from "../page-layouts/cart/Components/delivery-location/delivery-location";
+import Coupon from "../page-layouts/cart/Components/coupon/coupon";
+
+const Components = {
+  PriceBreakup,
+  DeliveryLocation,
+  Coupon,
+  ShareCart,
+  GstCard,
+  Comment,
+};
+export default Components;
