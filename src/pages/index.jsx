cb16f638b29a4b4458c6999a60271468fddf7973
Jigar Dafda <jigar.dafda@gmail.com>
2024-08-10 16:32:28 +0000

    [Auto Generated]

---
+++ src/pages/index.jsx
@@ -0,0 +1,22 @@
+import CartPage from "./cart";
+import SharedCart from "./shared-cart";
+import ProductListing from "./product-listing";
+import EditProfile from "./edit-profile";
+import ForgotPassword from "./forgot-password";
+import Login from "./login";
+import Register from "./register";
+import SetPassword from "./set-password";
+import CheckoutPage from "./checkout";
+
+const Pages = {
+  CartPage,
+  SharedCart,
+  ProductListing,
+  EditProfile,
+  ForgotPassword,
+  Login,
+  Register,
+  SetPassword,
+  CheckoutPage,
+};
+export default Pages;
