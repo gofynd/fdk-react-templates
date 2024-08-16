4d4528c6605f2ee07159561f3a3581e94009a210
Jigar Dafda <jigar.dafda@gmail.com>
2024-08-16 12:36:23 +0000

    [Auto Generated]

---
+++ src/components/quantity-control/quantity-control.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import * as styles from "./quantity-control.less";
+import SvgWrapper from "../core/svgWrapper/SvgWrapper";
+
+function QuantityControl({
+  count = 0,
+  onDecrementClick = () => {},
+  onIncrementClick = () => {},
+}) {
+  function handleDecrement(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    onDecrementClick(e);
+  }
+
+  function handleIncrement(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    onIncrementClick(e);
+  }
+
+  return (
+    <div className={styles.quantityCtrlContainer}>
+      <button className={styles.decreaseCount} onClick={handleDecrement}>
+        <div className={styles.svgContainer}>
+          <SvgWrapper svgSrc="decrease" />
+        </div>
+      </button>
+      <div className={styles.count}>{count}</div>
+      <button className={styles.increaseCount} onClick={handleIncrement}>
+        <span className={styles.svgContainer}>
+          <SvgWrapper svgSrc="increase" />
+        </span>
+      </button>
+    </div>
+  );
+}
+
+export default QuantityControl;
---
+++ src/pages/index.jsx
@@ -0,0 +1,24 @@
+import CartPage from "./cart";
+import SharedCart from "./shared-cart";
+import ProductListing from "./product-listing";
+import Wishlist from "./wishlist";
+import EditProfile from "./edit-profile";
+import ForgotPassword from "./forgot-password";
+import Login from "./login";
+import Register from "./register";
+import SetPassword from "./set-password";
+import CheckoutPage from "./checkout";
+
+const Pages = {
+  CartPage,
+  SharedCart,
+  ProductListing,
+  Wishlist,
+  EditProfile,
+  ForgotPassword,
+  Login,
+  Register,
+  SetPassword,
+  CheckoutPage,
+};
+export default Pages;
