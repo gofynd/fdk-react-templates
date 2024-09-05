694545868b4aee7e716816ffc7554dddd85ce863
Jigar Dafda <jigar.dafda@gmail.com>
2024-09-05 16:30:46 +0000

    [Auto Generated]

---
+++ src/pages/index.jsx
@@ -0,0 +1,30 @@
+import CartPage from "./cart";
+import SharedCart from "./shared-cart";
+import ProductListing from "./product-listing";
+import Wishlist from "./wishlist";
+import EditProfile from "./edit-profile";
+import ForgotPassword from "./forgot-password";
+import Login from "./login";
+import Register from "./register";
+import SetPassword from "./set-password";
+import CheckoutPage from "./checkout";
+import ProfileDetails from "./profile/profile-details";
+import Email from "./profile/email";
+import VerifyEmail from "./verify-email";
+
+const Pages = {
+  CartPage,
+  SharedCart,
+  ProductListing,
+  Wishlist,
+  EditProfile,
+  ForgotPassword,
+  Login,
+  Register,
+  SetPassword,
+  CheckoutPage,
+  ProfileDetails,
+  Email,
+  VerifyEmail,
+};
+export default Pages;
