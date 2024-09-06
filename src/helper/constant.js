d00df7cc85f9f3c7441a8efd17e36130e832db1e
Jigar Dafda <jigar.dafda@gmail.com>
2024-09-06 08:26:04 +0000

    [Auto Generated]

---
+++ src/helper/constant.js
@@ -0,0 +1,64 @@
+export const SINGLE_FILTER_VALUES = {
+  page_no: true,
+};
+
+export const ALL_PROFILE_MENU = [
+  {
+    key: "orders",
+    icon: "orders",
+    display: "My Orders",
+    link: "/profile/orders",
+    disabled_cart: false,
+  },
+  {
+    key: "phone",
+    icon: "call",
+    display: "Phone Number",
+    link: "/profile/phone",
+    disabled_cart: true,
+  },
+  {
+    key: "email",
+    icon: "email",
+    display: "Email Address",
+    link: "/profile/email",
+    staff: false,
+    disabled_cart: true,
+  },
+  {
+    key: "address",
+    icon: "address",
+    display: "My Address",
+    link: "/profile/address",
+    disabled_cart: false,
+  },
+  // {
+  //   key: "card",
+  //   icon: "card",
+  //   display: "My Cards",
+  //   link: "/profile/my-cards",
+  //   disabled_cart: false,
+  // },
+  // {
+  //   key: "reward_points",
+  //   icon: "refernearn",
+  //   display: "Refer and Earn",
+  //   link: "/profile/refer-earn",
+  //   disabled_cart: false,
+  // },
+];
+
+export const GENDER_OPTIONS = [
+  {
+    value: "male",
+    display: "Male",
+  },
+  {
+    value: "female",
+    display: "Female",
+  },
+  {
+    value: "unisex",
+    display: "Other",
+  },
+];
