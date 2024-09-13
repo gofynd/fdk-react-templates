7f8171c918f1a89edb93f84145c3de988055cb39
Jigar Dafda <jigar.dafda@gmail.com>
2024-09-13 06:44:35 +0000

    [Auto Generated]

---
+++ src/components/core/infinite-loader/infinite-loader.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useRef } from "react";
+import * as styles from "./infinite-loader.less";
+
+const InfiniteLoader = ({ children, isLoading, loader, loadMore, hasNext }) => {
+  const observer = useRef();
+  const lastElementRef = useRef(null);
+
+  useEffect(() => {
+    if (observer.current) observer.current.disconnect();
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && hasNext && !isLoading) {
+        loadMore();
+      }
+    });
+
+    if (lastElementRef.current)
+      observer.current.observe(lastElementRef.current);
+    return () => {
+      if (observer.current) observer.current.disconnect();
+    };
+  }, [isLoading, hasNext, loadMore]);
+
+  return (
+    <>
+      {children}
+      <div ref={lastElementRef}>{hasNext && (loader || <Loader />)}</div>
+    </>
+  );
+};
+
+const Loader = () => {
+  return (
+    <div className={styles.loaderContainer}>
+      <span className={styles.loaderIcon}>
+        <svg
+          width="100%"
+          height="auto"
+          viewBox="0 0 200 200"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 174C140.869 174 174 140.869 174 100C174 59.1309 140.869 26 100 26C59.1309 26 26 59.1309 26 100C26 140.869 59.1309 174 100 174Z"
+            stroke="#4E3F09"
+            strokeWidth="20"
+            strokeDasharray="348.72 120.24"
+          />
+        </svg>
+      </span>
+    </div>
+  );
+};
+
+export default InfiniteLoader;
