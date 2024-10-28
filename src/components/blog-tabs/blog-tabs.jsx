4c22294a8e0504fdd58cebb4b8be711b3e1f07af
Jigar Dafda <jigar.dafda@gmail.com>
2024-10-28 07:05:43 +0000

    [Auto Generated]

---
+++ src/components/blog-tabs/blog-tabs.jsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { FDKLink } from "fdk-core/components";
+import * as styles from "./blog-tabs.less";
+import FyImage from "../core/fy-image/fy-image";
+
+function BlogTabs({
+  show_filters,
+  show_recent_blog,
+  show_search,
+  show_tags,
+  show_top_blog,
+  fallback_image = "",
+  topViewedBlogs = [],
+  recentBlogs = [],
+}) {
+  const [sideTab, setSideTab] = useState(show_top_blog ? "tab1" : "");
+
+  if (
+    !show_filters &&
+    !show_recent_blog &&
+    !show_search &&
+    !show_tags &&
+    !show_top_blog
+  ) {
+    return null;
+  }
+
+  return (
+    <div className={`${styles.sideTabs}`}>
+      <div className={`${styles.sideTabs__menu}`} role="tablist">
+        {show_top_blog && (
+          <button
+            type="button"
+            className={sideTab === "tab1" ? `${styles.active}` : ""}
+            role="tab"
+            onClick={() => setSideTab("tab1")}
+          >
+            <div>Top viewed</div>
+          </button>
+        )}
+        {show_recent_blog && (
+          <button
+            type="button"
+            className={sideTab === "tab2" ? `${styles.active}` : ""}
+            role="tab"
+            onClick={() => setSideTab("tab2")}
+          >
+            <div>Recently Published</div>
+          </button>
+        )}
+      </div>
+
+      <div className={`${styles.sideTabs__content}`}>
+        {show_top_blog && topViewedBlogs?.length > 0 && sideTab === "tab1" && (
+          <div role="tabpanel">
+            <div className={`${styles.sideTabs__list}`} role="list">
+              {topViewedBlogs.map((blog, item) => (
+                <div role="listitem" key={blog?.id}>
+                  <FDKLink to={`/blog/${blog.slug}`} title={blog.title}>
+                    <div className={`${styles.blogHorizontal}`}>
+                      <FyImage
+                        src={blog?.feature_image?.secure_url || fallback_image}
+                        alt={blog.title}
+                        isLazyLoaded={false}
+                        sources={[{ width: 80 }]}
+                        placeholder={fallback_image}
+                        aspectRatio="auto"
+                        customClass={`${styles.blogHorizontal__image}`}
+                      />
+
+                      <div className={`${styles.blogHorizontal}`}>
+                        {blog.title}
+                      </div>
+                    </div>
+                  </FDKLink>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+        {show_recent_blog && recentBlogs?.length > 0 && sideTab === "tab2" && (
+          <div role="tabpanel">
+            <div className={`${styles.sideTabs__list}`} role="list">
+              {recentBlogs.map((blog, item) => (
+                <div role="listitem" key={blog?.id}>
+                  <FDKLink to={`/blog/${blog.slug}`} title={blog.title}>
+                    <div className={`${styles.blogHorizontal}`}>
+                      <FyImage
+                        src={blog?.feature_image?.secure_url || fallback_image}
+                        alt={blog.title}
+                        isLazyLoaded={false}
+                        sources={[{ width: 80 }]}
+                        placeholder={fallback_image}
+                        aspectRatio="auto"
+                        customClass={`${styles.blogHorizontal__image}`}
+                      />
+
+                      <div className={`${styles.blogHorizontal}`}>
+                        {blog.title}
+                      </div>
+                    </div>
+                  </FDKLink>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default BlogTabs;
