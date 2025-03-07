b986e4550a729398d790aa69a4bff2c8dab7d10b
Jigar Dafda <jigar.dafda@gmail.com>
2025-03-07 11:37:32 +0000

    [Auto Generated]

--- src/page-layouts/single-checkout/payment/checkout-payment.jsx
+++ src/page-layouts/single-checkout/payment/checkout-payment.jsx
@@ -3,6 +3,7 @@ import SvgWrapper from "../../../components/core/svgWrapper/SvgWrapper";
 import CheckoutPaymentContent from "./checkout-payment-content";
 import * as styles from "./checkout-payment.less";
 import CheckoutPaymentFailure from "./checkout-payment-failure";
+import { useMobile } from "../../../helper/hooks/useMobile";
 
 function CheckoutPayment({
   loader,
@@ -12,25 +13,46 @@ function CheckoutPayment({
   breakUpValues,
   onPriceDetailsClick,
   setCancelQrPayment,
+  onFailedGetCartShipmentDetails,
 }) {
   const [showFailedMessage, setShowFailedMessage] = useState(false);
   const [paymentErrHeading, setPaymentErrHeading] = useState("");
   const [paymentErrMsg, setPaymentErrMsg] = useState("");
   const [timerId, setTimerId] = useState(null);
+  const { errorMessage, setErrorMessage } = payment;
+  const isMobile = useMobile();
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.get("failed") === "true") {
+    if (errorMessage?.length > 0) {
+      handleShowFailedMessage({
+        failed: true,
+        paymentErrHeading: "Please try again later",
+        paymentErrMsg: errorMessage,
+      });
+      onFailedGetCartShipmentDetails();
+    } else if (urlParams.get("failed") === "true") {
       showPaymentOptions();
       handleShowFailedMessage({
         failed: true,
         paymentErrMsg: urlParams?.get("error"),
       });
+      onFailedGetCartShipmentDetails();
     }
-  }, [window.location.search]);
+  }, [errorMessage]);
+
+  const scrollToTop = () => {
+    if (isMobile) {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  };
 
   const handleShowFailedMessage = (errObj) => {
     if (errObj?.failed) {
+      scrollToTop();
       setShowFailedMessage(true);
       setPaymentErrHeading(errObj?.paymentErrHeading || "");
       setPaymentErrMsg(errObj?.paymentErrMsg || "");
@@ -57,6 +79,7 @@ function CheckoutPayment({
     setShowFailedMessage(false);
     setPaymentErrHeading("");
     setPaymentErrMsg("");
+    setErrorMessage("");
 
     const newUrlParams = new URLSearchParams(window.location.search);
     newUrlParams.delete("failed");
