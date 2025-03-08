1a5103f0fc0ba5f2914665fb5396e1e1c7128232
Jigar Dafda <jigar.dafda@gmail.com>
2025-03-08 04:22:19 +0000

    [Auto Generated]

--- src/components/blog-page/blog-page.jsx
+++ src/components/blog-page/blog-page.jsx
@@ -24,6 +24,7 @@ import HTMLContent from "../core/html-content/html-content";
 import BlogTabs from "../blog-tabs/blog-tabs";
 import BlogFooter from "../blog-footer/blog-footer";
 import { convertUTCDateToLocalDate } from "../../helper/utils";
+import { useLocation } from "react-router-dom";
 
 function BlogPage({
   contactInfo,
@@ -34,11 +35,15 @@ function BlogPage({
   isBlogDetailsLoading,
 }) {
   const params = useParams();
+  const location = useLocation();
   useEffect(() => {
     if (!blogDetails) {
-      getBlog(params?.slug);
+      const searchParams = new URLSearchParams(location.search);
+      const previewFlag = searchParams.get("__preview"); // Extract __preview if exists
+
+      getBlog(params?.slug, previewFlag ? true : false);
     }
-  }, [params?.slug]);
+  }, [params?.slug, location.search]);
 
   const containerRef = useRef(null);
 
