9985b8dd37756bb911d1e02b9128b44fabd0df06
Jigar Dafda <jigar.dafda@gmail.com>
2025-04-29 13:17:43 +0000

    [Auto Generated]

--- src/page-layouts/login/component/term-privacy/term-privacy.jsx
+++ src/page-layouts/login/component/term-privacy/term-privacy.jsx
@@ -2,18 +2,45 @@ import React from "react";
 import { FDKLink } from "fdk-core/components";
 import * as styles from "./term-privacy.less";
 
-function TermPrivacy() {
+function TermPrivacy({ onChange, checked }) {
+  const isRegisterPage =
+    typeof window !== "undefined" &&
+    window.location.pathname.includes("auth/register");
+
   return (
-    <div className={styles.midLinks}>
-      By continuing, I agree to the&nbsp;
-      <FDKLink to="/terms-and-conditions" target="_blank">
-        Terms of Service
-      </FDKLink>
-      &nbsp;&amp;&nbsp;
-      <FDKLink to="/privacy-policy" target="_blank">
-        Privacy Policy
-      </FDKLink>
-    </div>
+    <>
+      {isRegisterPage ? (
+          <div className={styles.midLinks}>
+            <input
+              className={styles.checkbox}
+              type="checkbox"
+              onChange={(e) => onChange?.(e.target.checked)}
+              checked={checked}
+            />
+            <span>
+              I agree to the&nbsp;
+              <FDKLink to="/terms-and-conditions" target="_blank">
+                Terms of Service
+              </FDKLink>
+              &nbsp;&amp;&nbsp;
+              <FDKLink to="/privacy-policy" target="_blank">
+                Privacy Policy
+              </FDKLink>
+            </span>
+          </div>
+      ) : (
+        <span className={styles.byContinuingText}>
+          By continuing, I agree to the&nbsp;
+          <FDKLink to="/terms-and-conditions" target="_blank">
+            Terms of Service
+          </FDKLink>
+          &nbsp;&amp;&nbsp;
+          <FDKLink to="/privacy-policy" target="_blank">
+            Privacy Policy
+          </FDKLink>
+        </span>
+      )}
+    </>
   );
 }
 
