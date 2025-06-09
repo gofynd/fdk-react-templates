162a18c29eeed4d30147a50378712677b02716f9
Jigar Dafda <jigar.dafda@gmail.com>
2025-06-09 11:53:02 +0000

    [Auto Generated]

--- src/page-layouts/plp/Components/add-to-cart/add-to-cart.jsx
+++ src/page-layouts/plp/Components/add-to-cart/add-to-cart.jsx
@@ -65,7 +65,10 @@ const AddToCart = ({
     const priceDataDefault = sizes?.price;
     if (selectedSize && !isEmptyOrNull(productPrice?.price)) {
       if (productPrice?.set) {
-        return currencyFormat(price_per_piece[key]) || "";
+        return currencyFormat(
+          price_per_piece[key], 
+          productPrice?.price?.currency_symbol || "", 
+          formatLocale(locale, countryCode, true)) || "";
       }
       const price = productPrice?.price || "";
       return currencyFormat(
