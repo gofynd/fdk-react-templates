5315e14079edba273a1091e24a175156cc606ca2
Jigar Dafda <jigar.dafda@gmail.com>
2025-06-11 10:03:03 +0000

    [Auto Generated]

--- src/page-layouts/single-checkout/payment/checkout-payment.jsx
+++ src/page-layouts/single-checkout/payment/checkout-payment.jsx
@@ -3,6 +3,7 @@ import SvgWrapper from "../../../components/core/svgWrapper/SvgWrapper";
 import CheckoutPaymentContent from "./checkout-payment-content";
 import * as styles from "./checkout-payment.less";
 
+
 import CheckoutPaymentFailure from "./checkout-payment-failure";
 import { useMobile } from "../../../helper/hooks/useMobile";
 import { useGlobalTranslation } from "fdk-core/utils";
@@ -14,17 +15,15 @@ function CheckoutPayment({
   showPaymentOptions,
   breakUpValues,
   onPriceDetailsClick,
-  setCancelQrPayment = () => {},
-  onFailedGetCartShipmentDetails = () => {},
-  customClassName,
-  isCouponApplied,
+  setCancelQrPayment,
+  onFailedGetCartShipmentDetails,
 }) {
   const { t } = useGlobalTranslation("translation");
   const [showFailedMessage, setShowFailedMessage] = useState(false);
   const [paymentErrHeading, setPaymentErrHeading] = useState("");
   const [paymentErrMsg, setPaymentErrMsg] = useState("");
   const [timerId, setTimerId] = useState(null);
-  const { errorMessage, setErrorMessage, enableLinkPaymentOption } = payment;
+  const { errorMessage, setErrorMessage } = payment;
   const isMobile = useMobile();
 
   useEffect(() => {
@@ -104,23 +103,15 @@ function CheckoutPayment({
   return (
     <>
       <div
-        className={`${styles.paymentContainer} ${!showPayment ? styles.hidePayment : ""} ${enableLinkPaymentOption ? styles.unsetMarginTop : ""}`}
+        className={`${styles.paymentContainer} ${!showPayment ? styles.hidePayment : ""}`}
       >
         {showPayment ? (
           <>
-            <div
-              className={`${styles.paymentHeaderSelect} ${customClassName} ${enableLinkPaymentOption ? styles.unsetBorder : ""}`}
-            >
-              {!enableLinkPaymentOption && (
-                <div className={`${styles.icon} ${styles["view-mobile-up"]}`}>
-                  <SvgWrapper svgSrc={"three-number"}></SvgWrapper>
-                </div>
-              )}
-              <div
-                className={`${styles.title} ${enableLinkPaymentOption ? styles.unsetPaddingLeft : ""}`}
-              >
-                {t("resource.checkout.select_payment_method")}
+            <div className={styles.paymentHeaderSelect}>
+              <div className={`${styles.icon} ${styles["view-mobile-up"]}`}>
+                <SvgWrapper svgSrc={"three-number"}></SvgWrapper>
               </div>
+              <div className={styles.title}>{t("resource.checkout.select_payment_method")}</div>
             </div>
             {showFailedMessage && (
               <div className={styles.paymentFailedHeader}>
@@ -139,15 +130,12 @@ function CheckoutPayment({
               breakUpValues={breakUpValues}
               removeDialogueError={hideFailedMessage}
               setCancelQrPayment={setCancelQrPayment}
-              isCouponApplied={isCouponApplied}
             ></CheckoutPaymentContent>
           </>
         ) : (
           <div className={styles.reviewHeaderUnselect}>
             <SvgWrapper svgSrc={"three-number"}></SvgWrapper>
-            <div className={styles.title}>
-              {t("resource.checkout.select_payment_method")}
-            </div>
+            <div className={styles.title}>{t("resource.checkout.select_payment_method")}</div>
           </div>
         )}
       </div>
