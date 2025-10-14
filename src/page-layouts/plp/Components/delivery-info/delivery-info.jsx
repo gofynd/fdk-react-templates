b276b2be1e2dc9e8b9c22444efc52df1380359c4
Jigar Dafda <jigar.dafda@gmail.com>
2025-10-14 09:09:59 +0000

    [Auto Generated]

--- src/page-layouts/plp/Components/delivery-info/delivery-info.jsx
+++ src/page-layouts/plp/Components/delivery-info/delivery-info.jsx
@@ -1,79 +1,99 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useState } from "react";
 import SvgWrapper from "../../../../components/core/svgWrapper/SvgWrapper";
+import { convertUTCDateToLocalDate } from "../../../../helper/utils";
 import * as styles from "./delivery-info.less";
 import FyButton from "../../../../components/core/fy-button/fy-button";
 import FyInput from "../../../../components/core/fy-input/fy-input";
-import { useGlobalTranslation } from "fdk-core/utils";
 
 function DeliveryInfo({
+  tat,
   pincode,
-  setPincode,
   pincodeErrorMessage,
-  setPincodeErrorMessage,
-  isServiceability,
-  tatMessage,
   checkPincode,
-  fulfillmentOptions,
-  availableFOCount,
+  setPincodeErrorMessage,
 }) {
-  const { t } = useGlobalTranslation("translation");
+  const [postCode, setPostCode] = useState(pincode || "");
+  const [tatMessage, setTatMessage] = useState("");
   const numberRegex = /^\d*$/;
+
+  useEffect(() => {
+    setPostCode(pincode);
+  }, [pincode]);
+
+  useEffect(() => {
+    if (postCode?.length > 5) {
+      getDeliveryDate();
+    }
+  }, [tat]);
 
   const changePostCode = (event) => {
     const value = event?.target?.value;
     if (numberRegex.test(Number(value))) {
-      setPincode(value);
+      setPostCode(value);
+      setTatMessage("");
       setPincodeErrorMessage("");
-      // setPincodeChecked(false);
+      setPincodeChecked(false);
     }
   };
 
-  const handleDeliveryAction = async () => {
-    await checkPincode(pincode);
-    //setPincodeChecked(true);
+  const getDeliveryDate = () => {
+    const options = {
+      weekday: "short",
+      month: "short",
+      day: "numeric",
+    };
+    const { min, max } = tat || {};
+
+    if (!min) {
+      return false;
+    }
+
+    const minDate = convertUTCDateToLocalDate(min, options);
+    const maxDate = convertUTCDateToLocalDate(max, options);
+    setTimeout(() => {
+      setTatMessage(
+        `Delivery ${
+          min === max ? `on ${minDate}` : `between ${minDate} - ${maxDate}`
+        }`
+      );
+    }, 1000);
   };
 
-  const foCount = useMemo(() => {
-    return fulfillmentOptions?.length || 0;
-  }, [fulfillmentOptions]);
+  const handleDeliveryAction = async () => {
+    await checkPincode(postCode);
+    setPincodeChecked(true);
+  };
 
   return (
     <div className={styles.deliveryInfo}>
-      {!isServiceability && (
-        <>
-          <h4 className={`${styles.deliveryLabel} b2`}>
-            {t("resource.common.address.select_delivery_location")}
-          </h4>
-          <div className={styles.delivery}>
-            <FyInput
-              autoComplete="off"
-              value={pincode}
-              placeholder={t("resource.product.check_delivery_time")}
-              inputClassName={styles.pincodeInput}
-              containerClassName={styles.pincodeInputContainer}
-              maxLength="6"
-              onChange={changePostCode}
-              inputVariant="no-border"
-              type="text"
-            />
-            <FyButton
-              variant="text"
-              className={styles.deliveryAction}
-              onClick={handleDeliveryAction}
-              disabled={pincode?.length !== 6}
-              endIcon={
-                <SvgWrapper svgSrc="delivery" className={styles.deliveryIcon} />
-              }
-            >
-              {t("resource.facets.check")}
-            </FyButton>
-          </div>
-        </>
-      )}
-
-      {!pincodeErrorMessage && availableFOCount === 1 && (
+      <h4 className={`${styles.deliveryLabel} b2`}>Select delivery location</h4>
+      <div className={styles.delivery}>
+        <FyInput
+          autoComplete="off"
+          value={postCode}
+          placeholder="Check delivery time"
+          inputClassName={styles.pincodeInput}
+          containerClassName={styles.pincodeInputContainer}
+          maxLength="6"
+          onChange={changePostCode}
+          inputVariant="no-border"
+          type="text"
+        />
+        <FyButton
+          variant="text"
+          className={styles.deliveryAction}
+          onClick={handleDeliveryAction}
+          disabled={postCode?.length !== 6}
+          endIcon={
+            <SvgWrapper svgSrc="delivery" className={styles.deliveryIcon} />
+          }
+        >
+          CHECK
+        </FyButton>
+      </div>
+      {!pincodeErrorMessage && (
         <div className={`${styles.deliveryDate} ${styles.dateInfoContainer}`}>
-          {tatMessage?.length > 0 && (
+          {postCode?.length === 6 && tatMessage?.length > 0 && (
             <>
               <SvgWrapper
                 svgSrc="delivery"
--- src/page-layouts/single-checkout/shipment/single-page-shipment.jsx
+++ src/page-layouts/single-checkout/shipment/single-page-shipment.jsx
@@ -2,11 +2,8 @@ import React from "react";
 import SvgWrapper from "../../../components/core/svgWrapper/SvgWrapper";
 import * as styles from "./single-page-shipment.less";
 import SingleShipmentContent from "./single-shipment-content";
-import {
-  useNavigate,
-  useGlobalTranslation,
-  useGlobalStore,
-} from "fdk-core/utils";
+import { useNavigate } from "react-router-dom";
+import { useGlobalStore } from "fdk-core/utils";
 import StickyPayNow from "../payment/sticky-pay-now/sticky-pay-now";
 import Shimmer from "../../../components/shimmer/shimmer";
 
@@ -18,20 +15,14 @@ function SinglePageShipment({
   showPaymentOptions,
   showShipment,
   showPayment,
+  isHyperlocal = false,
+  convertHyperlocalTat = () => {},
   loader,
   buybox = {},
-  availableFOCount,
   totalValue = "",
   onPriceDetailsClick = () => {},
-  customClassName,
-  isCartValid,
-  payment,
-  getDeliveryPromise,
-  redirectPaymentOptions,
 }) {
-  const { t } = useGlobalTranslation("translation");
   const navigate = useNavigate();
-  const { proceedToPay, getTotalValue, isLoading } = payment;
   const getShipmentCount = shipments?.length || 0;
 
   const editShipment = () => {
@@ -52,14 +43,12 @@ function SinglePageShipment({
                 <SvgWrapper svgSrc={"two-number"}></SvgWrapper>
               </div>
               <div className={styles.headerContainer}>
-                <div className={styles.orderSummary}>
-                  {t("resource.checkout.order_summary")}
-                </div>
+                <div className={styles.orderSummary}>Order Summary</div>
                 <div className={styles.shipment}>
                   {isShipmentLoading ? (
                     <Shimmer height="12px" width="120px" />
                   ) : (
-                    `${getShipmentCount} ${t(getShipmentCount > 1 ? "resource.common.shipments_plural" : "resource.common.shipment")}`
+                    `${getShipmentCount} shipment${getShipmentCount > 1 ? "s" : ""}`
                   )}
                 </div>
               </div>
@@ -67,26 +56,10 @@ function SinglePageShipment({
 
             <div className={styles.right}>
               <div className={styles.editCart} onClick={gotoCart}>
-                {t("resource.checkout.edit_cart_lower")}
+                Edit Cart
               </div>
-              <div
-                className={styles.proceedPay}
-                onClick={() => {
-                  if (getTotalValue?.() === 0) {
-                    proceedToPay("PP", {});
-                  } else {
-                    redirectPaymentOptions();
-                    showPaymentOptions();
-                  }
-                }}
-                style={{
-                  opacity: isLoading ? 0.5 : 1,
-                  pointerEvents: isLoading ? "none" : "auto",
-                }}
-              >
-                {getTotalValue?.() === 0
-                  ? "Place Order "
-                  : t("resource.checkout.proceed_to_pay")}
+              <div className={styles.proceedPay} onClick={showPaymentOptions}>
+                Proceed To Pay
               </div>
             </div>
           </div>
@@ -94,79 +67,47 @@ function SinglePageShipment({
             shipments={shipments}
             isShipmentLoading={isShipmentLoading}
             showPaymentOptions={showPaymentOptions}
+            isHyperlocal={isHyperlocal}
+            convertHyperlocalTat={convertHyperlocalTat}
             buybox={buybox}
-            availableFOCount={availableFOCount}
-            isCartValid={isCartValid}
-            getTotalValue={getTotalValue}
-            proceedToPay={proceedToPay}
-            isLoading={isLoading}
-            getDeliveryPromise={getDeliveryPromise}
-            redirectPaymentOptions={redirectPaymentOptions}
           ></SingleShipmentContent>
           <StickyPayNow
-            btnTitle={
-              getTotalValue?.() === 0
-                ? "PLACE ORDER"
-                : t("resource.checkout.proceed_to_pay_caps")
-            }
+            btnTitle="PROCEED TO PAY"
             onPriceDetailsClick={onPriceDetailsClick}
             value={totalValue}
-            disabled={isLoading || !isCartValid}
             proceedToPay={() => {
-              if (getTotalValue?.() === 0) {
-                proceedToPay("PP", {});
-              } else {
-                redirectPaymentOptions();
-                showPaymentOptions();
-                window?.scrollTo({
-                  top: 0,
-                });
-              }
+              showPaymentOptions();
+              window?.scrollTo({
+                top: 0,
+              });
             }}
           />
         </>
       ) : (
         <>
           {showPayment ? (
-            <div
-              className={`${styles.addressSelectedHeaderContainer} ${customClassName}`}
-            >
+            <div className={styles.addressSelectedHeaderContainer}>
               <div className={styles.leftSelected}>
                 <div className={styles.icon}>
                   <SvgWrapper svgSrc="checkmark"></SvgWrapper>
                 </div>
                 <div className={styles.deliverAdd}>
-                  <div className={styles.title}>
-                    {t("resource.checkout.order_summary")}
-                  </div>
+                  <div className={styles.title}>Order Summary</div>
                   <div className={styles.address}>
                     {getShipmentCount > 1
-                      ? getShipmentCount +
-                        ` ${t("resource.common.shipments_plural")}`
-                      : getShipmentCount + ` ${t("resource.common.shipments")}`}
+                      ? getShipmentCount + " shipments"
+                      : getShipmentCount + " shipment"}
                   </div>
                 </div>
               </div>
-              <div className={styles.orderEditContainer}>
-                <div className={styles.rightSelected} onClick={editShipment}>
-                  {t("resource.facets.edit")}
-                </div>
-                {getTotalValue?.() === 0 && (
-                  <button
-                    className={`${styles.commonBtn} ${styles.payBtn}`}
-                    onClick={() => proceedToPay("PP", {})}
-                  >
-                    PLACE ORDER
-                  </button>
-                )}
+              <div className={styles.rightSelected} onClick={editShipment}>
+                Edit
               </div>
             </div>
           ) : (
             <div className={styles.reviewHeaderUnselect}>
               <SvgWrapper svgSrc={"two-number"}></SvgWrapper>
-              <div className={styles.heading}>
-                {t("resource.checkout.order_summary")}
-              </div>
+              <div className={styles.heading}>Order Summary</div>
             </div>
           )}
         </>
