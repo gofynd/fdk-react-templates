7fc10a1769e67a4c5c436c11a27bbb065de8aa03
Jigar Dafda <jigar.dafda@gmail.com>
2025-12-03 06:39:12 +0000

    [Auto Generated]

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -42,6 +42,7 @@ function Checkout({
   isCouponValid,
   setIsCouponValid,
   inValidCouponData,
+  fileUpload = () => {},
 }) {
   const [cancelQrPayment, setCancelQrPayment] = useState(null);
   const [searchParams] = useSearchParams();
@@ -124,6 +125,7 @@ function Checkout({
           isCouponValid={isCouponValid}
           setIsCouponValid={setIsCouponValid}
           inValidCouponData={inValidCouponData}
+          fileUpload={fileUpload}
         ></CheckoutPayment>
       </div>
       <div className={styles.rightContainer}>
